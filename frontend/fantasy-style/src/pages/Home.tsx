2059311b48629231adb835d25936449cb25e0159
“Matvey <matveytytarenko25@gmail.com>
2024-10-13 19:19:32 +0200

    React Skelet

---
+++ frontend/fantasy-style/src/pages/Home.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import background from '../assets/bg.png'
+
+function Home() {
+  return (
+    <div className="container vw-1080 vh-720">
+        <p>Hdk;oksdfopse</p>
+    </div>
+  );
+}
+
+export default Home
